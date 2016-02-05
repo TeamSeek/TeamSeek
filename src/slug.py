6bdc45db3cc337f6d286c14d166ff91c2524a7bb
Nicholas Vargas <mail@nickvargas.net>
2016-02-05 11:28:24 -0800

    Make PageCache data static and move to cache.py

--- src/slug.py
+++ src/slug.py
@@ -3,6 +3,7 @@ import time
 import json
 import cherrypy
 import pystache
+from src.cache import PageCache
 
 
 """
@@ -13,61 +14,10 @@ Resolve all non mapped url targets
 (essentially just "/" or "/<username>/")
 """
 
-NO_CACHE = 'NO_CACHE' in os.environ
-
-def pagePath(pageName):
-    """ Return the path for template pages """
-    return os.path.join('views', pageName + '.html')
-
-class Page:
-    pageName = ""
-    fileData = ""
-    template = ""
-    lastUpdated = 0
-    def __init__(self, pageName):
-        """ Read data from filesystem """
-        self.pageName = pageName
-        self.updateFileData()
-        self.template = pystache.parse(unicode(self.fileData, 'utf-8'))
-    def updateFileData(self):
-        """ Read data from template pages """
-        with open(pagePath(self.pageName)) as f:
-            self.fileData = f.read()
-        self.lastUpdated = time.time()
-    def render(self, renderParam):
-        """ Render {{ mustache }} blocks """
-        if 'initial_data' not in renderParam:
-            renderParam['initial_data'] = json.dumps({});
-        return pystache.render(self.template, renderParam)
-    def fresh(self):
-        """ Return True if cached data is fresh """
-        if NO_CACHE:
-            return False
-        return os.path.getmtime(pagePath(self.pageName))<=self.lastUpdated
-
-class PageCache:
-    ram = {}
-    def removeOldItems(self):
-        """ Purge old items from cache """
-        pass
-    def getRaw(self, pageName):
-        """ Raw html document """
-        return self.get(pageName).fileData
-    def getTemplate(self, pageName):
-        """ Pystache template """
-        return self.get(pageName).template
-    def get(self, pageName):
-        """ Page object """
-        if 'pageName' not in self.ram:
-            self.ram[pageName] = Page(pageName)
-        if not self.ram[pageName].fresh():
-            self.ram[pageName].updateFileData()
-        return self.ram[pageName]
-
-
 cache = PageCache()
 
 def createPage(session, params):
+    # Render /create page
     initial_data = {'user':session.get('user')}
     return cache.get('layout').render({
         'page_body':cache.getRaw('create'),
