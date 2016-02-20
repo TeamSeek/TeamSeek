3e990d70761a34f6c96f99ef014f987d2ce0167c
Dat Nguyen <nothing.16991@yahoo.com>
2016-02-19 20:04:27 -0500

    Add project's comments table into database

--- misc/schema.sql
+++ misc/schema.sql
@@ -3,20 +3,20 @@ CREATE TABLE users (
 	username VARCHAR NOT NULL,
 	email VARCHAR,
 	join_date DATE NOT NULL
-	);
+);
 
 CREATE TABLE user_extras (
 	user_id INT PRIMARY KEY,
     full_name: VARCHAR,
 	bio TEXT,
 	avatar VARCHAR
-	);
+);
 
 CREATE TABLE user_skills (
     user_id INT NOT NULL,
     skill VARCHAR NOT NULL,
     level VARCHAR DEFAULT 'Beginner'
-    );
+);
 
 CREATE TABLE project_info (
 	project_id SERIAL PRIMARY KEY,
@@ -26,23 +26,31 @@ CREATE TABLE project_info (
 	long_desc TEXT,
 	last_edit DATE,
 	posted_date DATE NOT NULL
-	);
+);
 
 CREATE TABLE project_skills (
     project_id INT NOT NULL,
     skill VARCHAR NOT NULL
-    );
+);
 
 CREATE TABLE project_members (
     project_id INT NOT NULL,
     member VARCHAR NOT NULL
-    );
+);
 
 CREATE TABLE project_extras (
 	project_id INT NOT NULL,
 	update TEXT,
 	git_link VARCHAR
-	);
+);
+
+CREATE TABLE project_cmts (
+    id SERIAL PRIMARY KEY,
+    project_id INT NOT NULL,
+    poster_id INT NOT NULL,
+    cmt TEXT,
+    cmt_time timestamp
+);
 
 CREATE TABLE applications (
     id SERIAL PRIMARY KEY,
@@ -50,13 +58,13 @@ CREATE TABLE applications (
 	applicant_id INT NOT NULL,
     status VARCHAR DEFAULT 'pending',
 	date_applied DATE NOT NULL
-	);
+);
 
 CREATE TABLE skills (
     name VARCHAR NOT NULL,
     approved BOOLEAN DEFAULT False,
     count INT DEFAULT 0
-    );
+);
 
 CREATE TABLE notifications (
     id SERIAL PRIMARY KEY,
@@ -66,14 +74,14 @@ CREATE TABLE notifications (
     read BOOLEAN DEFAULT False,
     deleted BOOLEAN DEFAULT False,
     created_date DATE NOT NULL
-    );
+);
 
 CREATE TABLE notification_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
     description TEXT,
     template TEXT
-    );
+);
 
 CREATE TABLE invitations (
     id SERIAL PRIMARY KEY,
